6e74c3ff0cb01bcaff8da1f56ce5724967d7117b
rjonathans <49173110+rjonathans@users.noreply.github.com>
2019-05-04 23:31:18 -0300

    criando os métodos

--- src/br/com/digitalhouse/DigitalHouseManager.java
+++ src/br/com/digitalhouse/DigitalHouseManager.java
@@ -5,34 +5,18 @@ import java.util.List;
 
 public class DigitalHouseManager {
     //Attributes
-    private List<Aluno> alunosMatriculados = new ArrayList<>();
-    private List<Professor> professoresCadastrados = new ArrayList<>();
+    private List<Aluno> alunos = new ArrayList<>();
+    private List<Professor> professores = new ArrayList<>();
     private List<Matricula> matriculas = new ArrayList<>();
     private List<Curso> cursos = new ArrayList<>();
 
     //Methods
     public void registrarCurso(Curso curso) {
-        this.cursos.add(curso);
-
+        this.getCursos().add(curso);
+        System.out.println("REGISTRANDO....\n" + curso + "\nCURSO REGISTRADO COM SUCESSO.\n"+"\n*****************\n");
     }
 
     public void excluirCurso(Integer codigoCurso) {
-
-    }
-
-    public void registrarProfessorTitular(String nome, String sobrenome, Integer codDeProfessor, String especialidade) {
-
-    }
-
-    public void excluirProfessor(Integer codigoProfessor) {
-
-    }
-
-    public void registrarAluno(Integer codigoAluno, Integer codigoCurso) {
-
-    }
-
-    public void alocarProfessores(Integer codigoCurso, Integer codigoProfessorTitular, Integer codigoProfessorAdjunto) {
 
     }
 
@@ -40,5 +24,54 @@ public class DigitalHouseManager {
 
     }
 
+    public void registrarProfessorTitular(Professor professores) {
+        this.professores.add(professores);
+        System.out.print("\nREGISTRANDO....\n" + professores+"\n" + "\nPROFESSOR REGISTRADO COM SUCESSO.\n"+"\n*****************\n");
+    }
 
+    public void excluirProfessor(Integer codigoProfessor) {
+
+    }
+
+    public void registrarAluno(Aluno aluno) {
+        this.getAlunos().add(aluno);
+        System.out.println("REGISTRANDO....\n" + aluno + "\nALUNO REGISTRADO COM SUCESSO.\n"+"\n*****************\n");
+    }
+
+    public void alocarProfessores(Integer codigoCurso, Integer codigoProfessorTitular, Integer codigoProfessorAdjunto) {
+
+    }
+
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public List<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(List<Professor> professores) {
+        this.professores = professores;
+    }
+
+    public List<Matricula> getMatriculas() {
+        return matriculas;
+    }
+
+    public void setMatriculas(List<Matricula> matriculas) {
+        this.matriculas = matriculas;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
 }
